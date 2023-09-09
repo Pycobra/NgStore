2bcf96ed27fe10431a45dc8ca13d5d98d2bb7c59
Bright Orji <megabright007@gmail.com>
2023-09-09 19:53:15 +0100

    This is a first commit

---
+++ apps/checkout/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from django.conf.urls import url
+
+from . import views
+
+app_name = "checkout_"
+
+urlpatterns = [
+    path("delivery-choices", views.deliverychoices, name="delivery_choice"),
+    path("cart_update_delivery/", views.cart_update_delivery, name="cart_update_delivery"),
+    path("delivery_address/", views.delivery_address, name="delivery_address"),
+    path("payment_selection/", views.payment_selection, name="payment_selection"),
+    path("complete-my-payment", views.complete_payment, name="complete_payment2"),
+    path("authenticating-user-details", views.user_details_authenticated, name="user_details_authenticated"),
+    #url(r'^create_order/(?P<ref>\d*)$', views.complete_payment, name='complete_payment2')
+    #path("payment_successful/", views.payment_successful, name="payment_successful"),
+]
