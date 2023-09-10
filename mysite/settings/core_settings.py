af50ed2a70e5f0bc7d332b0418b3d2ddad614827
Brightson Orji <84877175+Pycobra@users.noreply.github.com>
2023-09-10 14:37:12 +0100

    Update core_settings.py

--- mysite/settings/core_settings.py
+++ mysite/settings/core_settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "ng-store-4bvbdy90t-pycobra.vercel.app"]
 
 
 AUTH_USER_MODEL='account.UserBase'
